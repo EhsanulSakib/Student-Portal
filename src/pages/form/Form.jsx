6a39db205f902ad6c8ea3befa2ee9766e8e84503
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-06-26 08:17:24 +0600

    student form

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -103,20 +103,156 @@ const Form = () => {
 
 
     return (
-        <div className='mt-4 w-11/12 m-auto'>
-            <h1 className='text-3xl text-center font-bold'>Student Registration Form</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="first_name">First Name</label>
-                    <input
-                        id="first_name"
-                        name="first_name"
-                        type="text"
-                        value={formValues.first_name}
-                        onChange={handleChange}
-                    />
-                    {errors.first_name && <div>{errors.first_name}</div>}
+        <div className='mt-4 w-11/12 m-auto bg-slate-100 p-4 px-8 shadow-sm rounded-sm'>
+            <h1 className='text-3xl text-center font-bold text-blue-600'>Student Form</h1>
+            <form onSubmit={handleSubmit} className='my-4'>
+                <div className='flex flex-col md:flex-row justify-between gap-10'>
+                    <div className='flex flex-1 flex-col gap-4'>
+                        <div className='flex flex-col gap-2 w-full'>
+                            <label htmlFor="first_name" className='font-bold text-lg w-full'>First Name {errors.first_name && <span className='text-red-400 text-base font-medium'>({errors.first_name})</span>}</label>
+                            <input
+                                id="first_name"
+                                name="first_name"
+                                type="text"
+                                value={formValues.first_name}
+                                onChange={handleChange}
+                                className='p-2 border border-gray-400 w-full'
+                            />
+
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="last_name" className='font-bold text-lg w-full'>Last Name {errors.last_name && <span className='text-red-400 text-base font-medium'>({errors.last_name})</span>}</label>
+                            <input
+                                id="last_name"
+                                name="last_name"
+                                type="text"
+                                value={formValues.last_name}
+                                onChange={handleChange}
+                            />
+
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label className='font-bold text-lg w-full'>Gender {errors.gender && <span className='text-red-400 text-base font-medium'>({errors.gender})</span>}</label>
+                            <div className='flex gap-2'>
+                                <label className='flex gap-2'>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="Male"
+                                        checked={formValues.gender === 'Male'}
+                                        onChange={handleChange}
+                                    />
+                                    Male
+                                </label>
+                                <label className='flex gap-2'>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="Female"
+                                        checked={formValues.gender === 'Female'}
+                                        onChange={handleChange}
+                                    />
+                                    Female
+                                </label>
+                                <label className='flex gap-2'>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="Other"
+                                        checked={formValues.gender === 'Other'}
+                                        onChange={handleChange}
+                                    />
+                                    Other
+                                </label>
+                            </div>
+
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="address" className='font-bold text-lg w-full'>Address {errors.address && <span className='text-red-400 text-base font-medium'>({errors.address})</span>}</label>
+                            <input
+                                id="address"
+                                name="address"
+                                type="text"
+                                value={formValues.address}
+                                onChange={handleChange}
+                            />
+
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="phone_no" className='font-bold text-lg w-full'>Phone Number {errors.phone_no && <span className='text-red-400 text-base font-medium'>({errors.phone_no})</span>}</label>
+                            <input
+                                id="phone_no"
+                                name="phone_no"
+                                type="text"
+                                value={formValues.phone_no}
+                                onChange={handleChange}
+                            />
+
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="email" className='font-bold text-lg w-full'>Email {errors.email && <span className='text-red-400 text-base font-medium'>({errors.email})</span>}</label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                value={formValues.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="vio_details" className='font-bold text-lg w-full'>Violations Details</label>
+                            <textarea
+                                id="vio_details"
+                                name="vio_details"
+                                value={formValues.vio_details}
+                                onChange={handleChange}
+                                className='min-h-32 max-h-32 p-2'
+                            />
+                            {errors.vio_details && <div>{errors.vio_details}</div>}
+                        </div>
+
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="thana" className='font-bold text-lg w-full'>Thana</label>
+                            <select
+                                id="thana"
+                                name="thana"
+                                value={formValues.thana}
+                                onChange={handleChange}
+                            >
+                                <option value="">Select</option>
+                                <option value="Thana1">Khilgaon</option>
+                                <option value="Thana2">Rampura</option>
+                                <option value="Thana3">Badda</option>
+                                <option value="Thana3">Gulshan</option>
+                                <option value="Thana3">Mirpur</option>
+                                <option value="Thana3">Tajgaon</option>
+                                <option value="Thana3">Ramna</option>
+                            </select>
+                            {errors.thana && <div>{errors.thana}</div>}
+                        </div>
+                    </div>
+
+
+                    <div className='flex flex-col gap-4 w-full md:w-2/5'>
+                        <label htmlFor="photo" className='font-bold text-lg w-full'>Photo</label>
+                        {photoPreview && <img src={photoPreview} alt="Photo Preview" />}
+                        <input
+                            id="photo"
+                            name="photo"
+                            type="file"
+                            onChange={handlePhotoChange}
+                        />
+                        {errors.photo && <div>{errors.photo}</div>}
+                    </div>
                 </div>
+
+                <button type="submit" className='btn bg-blue-400 hover:bg-blue-600 text-white font-bold w-full mt-6 text-xl'>Submit</button>
             </form>
         </div>
     );
